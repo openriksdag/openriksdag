e9652bdd396b616024ce7fc18072ada43fe1478e
Ella <ella.wg@hotmail.com>
2020-02-26 16:46:49 +0100

    add riksdagChart

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.css";
+import RiksdagChart from "./components/RiksdagChart";
 
 function App() {
+  const data = [
+    { label: "CDU", value: 10 },
+    { label: "SPD", value: 15 },
+    { label: "Die Grünen", value: 8 },
+    { label: "Die Mitte", value: 1 },
+    { label: "Frei Wähler", value: 3 }
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <p>Open Riksdag</p>
+        <RiksdagChart
+          data={data}
+          width={400}
+          height={400}
+          innerRadius={80}
+          outerRadius={200}
+        />
       </header>
     </div>
   );

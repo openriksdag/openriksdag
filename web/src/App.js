5caacbb809ed3a61b81946f2f6ff6b6637f000e4
Ella <ella.wg@hotmail.com>
2020-02-26 17:10:18 +0100

    add layer pie chart

--- web/src/App.js
+++ web/src/App.js
@@ -3,23 +3,10 @@ import "./App.css";
 import RiksdagChart from "./components/RiksdagChart";
 
 function App() {
-  const data = [
-    { label: "CDU", value: 10 },
-    { label: "SPD", value: 15 },
-    { label: "Die Grünen", value: 8 },
-    { label: "Die Mitte", value: 1 },
-    { label: "Frei Wähler", value: 3 }
-  ];
   return (
     <div className="App">
       <header className="App-header">
-        <RiksdagChart
-          data={data}
-          width={400}
-          height={400}
-          innerRadius={80}
-          outerRadius={200}
-        />
+        <RiksdagChart />
       </header>
     </div>
   );
--- web/src/components/RiksdagChart.jsx
+++ web/src/components/RiksdagChart.jsx
@@ -1,52 +1,36 @@
 import React from "react";
-import * as d3 from "d3";
-
-const Arc = ({ data, index, createArc, colors, format }) => (
-  <g key={index} className="arc">
-    <path className="arc" d={createArc(data)} fill={colors(index)} />
-    <text
-      transform={`translate(${createArc.centroid(data)})`}
-      textAnchor="middle"
-      alignmentBaseline="middle"
-      fill="white"
-      fontSize="10"
-    >
-      {format(data.value)}
-    </text>
-  </g>
-);
+import PieChart from "./PieChart";
+import "./RiksdagsChart.css";
 
 const RiksdagChart = props => {
-  const { innerRadius, outerRadius, height, width, data } = props;
-  const createPie = d3
-    .pie()
-    .startAngle(-90 * (Math.PI / 180))
-    .endAngle(90 * (Math.PI / 180))
-    .value(d => d.value)
-    .sort(null);
-  const createArc = d3
-    .arc()
-    .innerRadius(innerRadius)
-    .outerRadius(outerRadius);
-  const colors = d3.scaleOrdinal(d3.schemeCategory10);
-  const format = d3.format(".2f");
-  const pieData = createPie(data);
-
+  const data = [
+    { label: "CDU", value: 10 },
+    { label: "SPD", value: 15 },
+    { label: "Die Grünen", value: 8 },
+    { label: "Die Mitte", value: 1 },
+    { label: "Frei Wähler", value: 3 }
+  ];
   return (
-    <svg width={width} height={height}>
-      <g transform={`translate(${outerRadius} ${outerRadius})`}>
-        {pieData.map((d, i) => (
-          <Arc
-            key={i}
-            data={d}
-            index={i}
-            createArc={createArc}
-            colors={colors}
-            format={format}
-          />
-        ))}
-      </g>
-    </svg>
+    <div class="pie-container">
+      <div class="pie-chart">
+        <PieChart
+          data={data}
+          width={250}
+          height={120}
+          innerRadius={100}
+          outerRadius={120}
+        />
+      </div>
+      <div class="pie-chart two">
+        <PieChart
+          data={data}
+          width={230}
+          height={100}
+          innerRadius={80}
+          outerRadius={100}
+        />
+      </div>
+    </div>
   );
 };
 

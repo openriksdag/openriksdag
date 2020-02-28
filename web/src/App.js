e2543d87791d28508011d42797056f27e5c36a3e
JOhohannes Loor <johannes.loor@gmail.com>
2020-02-28 11:30:22 +0100

    Moved layout to components

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import RiksdagChart from "./components/RiksdagChart";
-import Layout from "./layout.js";
+import Layout from "./components/layout.js";
 
 function App() {
   return (

6d8a91f19811b8284c1836a11cef20b062de89c9
Ella <ella.wg@hotmail.com>
2020-02-28 10:07:46 +0100

    Add party colors

--- web/src/components/RiksdagChart.jsx
+++ web/src/components/RiksdagChart.jsx
@@ -4,30 +4,46 @@ import "./RiksdagsChart.css";
 
 const RiksdagChart = props => {
   const data = [
-    { label: "CDU", value: 10 },
-    { label: "SPD", value: 15 },
-    { label: "Die Grünen", value: 8 },
-    { label: "Die Mitte", value: 1 },
-    { label: "Frei Wähler", value: 3 }
+    { label: "S", value: 100 },
+    { label: "M", value: 70 },
+    { label: "SD", value: 62 },
+    { label: "C", value: 31 },
+    { label: "V", value: 27 },
+    { label: "KD", value: 22 },
+    { label: "L", value: 19 },
+    { label: "MP", value: 16 }
   ];
+  const partyColors = [
+    "#EE1F21",
+    "#32ABFF",
+    "#DBBE13",
+    "#017A26",
+    "#9B0100",
+    "#1F3B96",
+    "#3D6BFF",
+    "#53a045"
+  ];
+
   return (
-    <div class="pie-container">
-      <div class="pie-chart">
+    <div className="pie-container">
+      <div className="pie-chart">
         <PieChart
           data={data}
           width={250}
           height={120}
-          innerRadius={100}
+          innerRadius={99}
           outerRadius={120}
+          partyColors={partyColors}
         />
       </div>
-      <div class="pie-chart two">
+      <div className="pie-chart two">
         <PieChart
           data={data}
           width={230}
           height={100}
           innerRadius={80}
           outerRadius={100}
+          partyColors={partyColors}
         />
       </div>
     </div>

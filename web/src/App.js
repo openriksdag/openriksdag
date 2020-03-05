c57d7e090d570d081803c1a7139d06ac6ca9ff45
Yoav Luft <yoav.luft@prototyp.se>
2020-03-05 12:21:31 +0100

    Add redux and use it to highlight representatives

--- web/src/App.js
+++ web/src/App.js
@@ -2,19 +2,17 @@ import React, {Component} from 'react';
 import RiksdagChart from "./components/RiksdagChart/RiksdagChart";
 import Layout from "./components/layout";
 import ChartWrapper from "./ChartWrapper";
+import {useSelector} from "react-redux"
 
-import peopleData from "./data/people.json";
 
-
-class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <ChartWrapper/>
-        <RiksdagChart people={peopleData}/>
-      </Layout>
-    )
-  }
+function App(props) {
+  const {peopleData, hovered} = useSelector(({peopleData, hovered}) => ({peopleData, hovered}))
+  return (
+    <Layout>
+      <ChartWrapper/>
+      <RiksdagChart people={peopleData} hovered={hovered}/>
+    </Layout>
+  )
 }
 
 export default App;

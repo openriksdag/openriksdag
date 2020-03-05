a179d100b39900532036e7719575922ad75f46c7
JOhohannes Loor <johannes.loor@gmail.com>
2020-03-05 14:33:53 +0100

    Added missed import

--- web/src/App.js
+++ web/src/App.js
@@ -2,30 +2,34 @@ import React, { Component } from "react";
 import RiksdagChart from "./components/RiksdagChart/RiksdagChart";
 import Layout from "./components/layout";
 import ChartWrapper from "./ChartWrapper";
-import {useSelector} from "react-redux"
+import MotionsSquare from "./components/MotionsSquare";
+import { useSelector } from "react-redux";
 
 import propoData from "./data/propositions.json";
 import motionsData from "./data/motions.json";
 
 function App(props) {
-  const {peopleData, hovered} = useSelector(({peopleData, hovered}) => ({peopleData, hovered}))
+  const { peopleData, hovered } = useSelector(({ peopleData, hovered }) => ({
+    peopleData,
+    hovered
+  }));
   return (
     <Layout>
-      <ChartWrapper/>
-      <RiksdagChart people={peopleData} hovered={hovered}/>
+      <RiksdagChart people={peopleData} hovered={hovered} />
       <MotionsSquare
-          type="Motions"
-          description="Motions & Proposals"
-          data={motionsData}
-        />
-        <MotionsSquare
-          type="Proposals"
-          description="Proposals for decision"
-          data={propoData}
-          reverse
-        />
+        type="Motions"
+        description="Motions & Proposals"
+        data={motionsData}
+      />
+      <ChartWrapper />
+      <MotionsSquare
+        type="Proposals"
+        description="Proposals for decision"
+        data={propoData}
+        reverse
+      />
     </Layout>
-  )
+  );
 }
 
 export default App;

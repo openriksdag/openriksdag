e18aa333b21ccd8dff7e0c6ed7c365c3054a4345
JOhohannes Loor <johannes.loor@gmail.com>
2020-03-06 16:39:20 +0100

    Put title in motions

--- web/src/components/MotionChart/MotionsChart.jsx
+++ web/src/components/MotionChart/MotionsChart.jsx
@@ -5,55 +5,53 @@ import { useDispatch, useSelector } from "react-redux";
 import { ChangeHover, Hovered } from "../../state/state";
 
 const Title = props => {
-  const { hovered, type } = props;
-  return Hovered.case(hovered, {
-    Motion: ({ data }) =>
-      type === "Motions" && (
-        <a href={data.attachments[0].url} id="title">
+  const { data, type, dispatch, hovered } = props;
+  const handleMouseLeave = () => dispatch(ChangeHover(Hovered.Nothing()));
+  const handleMouseEnter = () =>
+    dispatch(
+      ChangeHover(
+        type === "Motions" ? Hovered.Motion(data) : Hovered.Proposition(data)
+      )
+    );
+  if (data.attachments[0]) {
+    return (
+      <div
+        key={data.dok_id}
+        className="motion"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <a target="_blank" href={data.attachments[0].url}>
           {data.attachments[0].titel}
         </a>
-      ),
-    Proposition: ({ data }) =>
-      type === "Proposals" && (
-        <a href={data.attachments[0].url} id="title">
-          {data.attachments[0].titel}
-        </a>
-      ),
-    otherwise: () => null
-  });
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
-
 const MotionsChart = props => {
   const { type, description, reverse, data } = props;
   const dispatch = useDispatch();
   const hovered = useSelector(state => state.hovered);
   const motions = useMemo(() => values(data), [data]);
-  const handleMouseLeave = () => dispatch(ChangeHover(Hovered.Nothing()));
+
   return (
     <div className={reverse ? "wrapper reverse" : "wrapper"}>
       <div className={reverse ? "infoContainer flipText" : "infoContainer"}>
         <span id="description">{description}</span>
-        <Title hovered={hovered} type={type}></Title>
       </div>
 
       <figure className="squareContainer">
         <span className="motionType">{type}</span>
         <div className="motionContainer">
-          {motions.map((d, i) => (
-            <div
-              key={i}
-              className="motion"
-              onMouseEnter={() =>
-                dispatch(
-                  ChangeHover(
-                    type === "Motions"
-                      ? Hovered.Motion(d)
-                      : Hovered.Proposition(d)
-                  )
-                )
-              }
-              onMouseLeave={handleMouseLeave}
-            ></div>
+          {motions.map(d => (
+            <Title
+              data={d}
+              type={type}
+              dispatch={dispatch}
+              hovered={hovered}
+            ></Title>
           ))}
         </div>
       </figure>

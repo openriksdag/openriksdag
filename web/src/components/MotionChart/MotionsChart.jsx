b9d3294ae45e784af22ee7524a94917916922b7c
Yoav Luft <yoav.luft@prototyp.se>
2020-03-07 16:39:05 +0100

    Improve performance of MotionsChart.jsx by use of black magic

--- web/src/components/MotionChart/MotionsChart.jsx
+++ web/src/components/MotionChart/MotionsChart.jsx
@@ -4,23 +4,16 @@ import {values} from "ramda";
 import {useDispatch} from "react-redux";
 import {ChangeHover, Hovered} from "../../state/state";
 import {isInDocument, isReferencedIn} from "../../relation-helpers"
+import {CellMeasurer, CellMeasurerCache, List} from "react-virtualized"
 
 const Title = memo(props => {
-  const {data, type} = props;
-  const dispatch = useDispatch()
-  const handleMouseLeave = useCallback(() => dispatch(ChangeHover(Hovered.Nothing())), [dispatch]);
-  const handleMouseEnter = useCallback(() =>
-    dispatch(
-      ChangeHover(
-        type === "Motions" ? Hovered.Motion(data) : Hovered.Proposition(data)
-      )
-    ), [type, data, dispatch]);
+  const {data, type, handleMouseOver, handleMouseLeave} = props;
   if (data.attachments[0]) {
     return (
       <div
         key={data.dok_id}
         className="motion"
-        onMouseEnter={handleMouseEnter}
+        onMouseEnter={handleMouseOver}
         onMouseLeave={handleMouseLeave}
       >
         <a target="_blank" rel="noopener noreferrer" href={data.attachments[0].url}>
@@ -42,9 +35,25 @@ const byHovered = (currentContainer, hovered) => (doc) =>
     Nothing: () => true
   })
 
+const cache = new CellMeasurerCache({
+  defaultWidth: 144,
+  minHeight: 16,
+  fixedWidth: true
+})
+
 const MotionsChart = props => {
   const {type, description, reverse, data, hovered} = props;
   const motions = useMemo(() => values(data), [data]);
+  const filteredMotions = motions.filter(byHovered(type, hovered))
+
+  const dispatch = useDispatch()
+  const handleMouseLeave = useCallback(() => dispatch(ChangeHover(Hovered.Nothing())), [dispatch]);
+  const handleMouseOver = (data, type) => () =>
+    dispatch(
+      ChangeHover(
+        type === "Motions" ? Hovered.Motion(data) : Hovered.Proposition(data)
+      )
+    )
 
   return (
     <div className={reverse ? "wrapper reverse" : "wrapper"}>
@@ -53,17 +62,33 @@ const MotionsChart = props => {
       </div>
 
       <figure className="squareContainer">
-        <span className="motionType">{type}</span>
         <div className="motionContainer">
-          {motions.filter(byHovered(type, hovered)).map(d => (
-            <Title
-              key={d.dok_id}
-              data={d}
-              type={type}
-              hovered={hovered}
-            />
-          ))}
+          <List
+            height={165}
+            width={144}
+            rowHeight={cache.rowHeight}
+            deferredMeasurementCache={cache}
+            rowCount={filteredMotions.length}
+            rowRenderer={({index, key, parent}) =>
+              <CellMeasurer
+                key={key}
+                parent={parent}
+                columnIndex={0}
+                rowIndex={index}
+                cache={cache}
+              >
+                <Title
+                  key={key}
+                  data={filteredMotions[index]}
+                  type={type}
+                  hovered={hovered}
+                  handleMouseOver={handleMouseOver(filteredMotions[index], type)}
+                  handleMouseLeave={handleMouseLeave}
+                />
+              </CellMeasurer>}
+          />
         </div>
+        <span className="motionType">{type}</span>
       </figure>
     </div>
   );

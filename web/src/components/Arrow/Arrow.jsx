523b556ee7ac2f05a32508dcef229b7763442981
JOhohannes Loor <johannes.loor@gmail.com>
2020-03-12 12:49:10 +0100

    Fixed arrows

--- web/src/components/Arrow/Arrow.jsx
+++ web/src/components/Arrow/Arrow.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import './Arrow.css'
+import "./Arrow.css";
 
-
-const Arrow = ({index, shownFor, hovered, selected}) => {
-
+const Arrow = ({ index, shownFor, hovered, selected }) => {
   const data = [
-    {x0: 130, y0: 10, x1: 30, y1: 30, x2: 10, y2: 100},
-    {x0: 10, y0: 10, x1: 30, y1: 70, x2: 130, y2: 100},
-    {x0: 10, y0: 100, x1: 120, y1: 70, x2: 130, y2: 10},
-    {x0: 130, y0: 100, x1: 100, y1: 30, x2: 10, y2: 10},
-    {x0: 130, y0: 60, x1: 60, y1: 30, x2: 10, y2: 60},
-  ]
+    { x0: 130, y0: 10, x1: 30, y1: 30, x2: 10, y2: 100 },
+    { x0: 10, y0: 10, x1: 30, y1: 70, x2: 130, y2: 100 },
+    { x0: 10, y0: 100, x1: 120, y1: 70, x2: 130, y2: 10 },
+    { x0: 130, y0: 100, x1: 100, y1: 30, x2: 10, y2: 10 },
+    { x0: 130, y0: 60, x1: 60, y1: 30, x2: 10, y2: 60 }
+  ];
 
-  const hoveredItem = hovered[shownFor]
-  const selectedItem = selected[shownFor]
-  const label = (item) => ({
-    'representative': `${item.first_name} ${item.last_name} proposed:`,
-    'motion': `Motion ${item.dok_id} was discussed in:`,
-    'committee': `${item} brought to decision:`,
-    'proposition': `Proposition ${item.dok_id} was brought to decision:`
-  }[shownFor])
+  const hoveredItem = hovered[shownFor];
+  const selectedItem = selected[shownFor];
+  const label = item =>
+    ({
+      representative: `${item.first_name} ${item.last_name} signed these motions`,
+      motion: `The motion was discussed in:`,
+      committee: `The ${item} sent these proposals for decision`,
+      proposition: `The proposal was was then voted on`
+    }[shownFor]);
 
-  const {x0, y0, x1, y1, x2, y2} = data[index];
+  const { x0, y0, x1, y1, x2, y2 } = data[index];
 
   return (
     <div className={`arrow arrow-${shownFor}`}>
@@ -34,33 +33,52 @@ const Arrow = ({index, shownFor, hovered, selected}) => {
               refY="6"
               markerWidth="40"
               markerHeight="40"
-              markerUnits="userSpaceOnUse"
+              markerUnits="strokeWidth"
               orient="auto"
             >
               <path
                 d="M 0 0 12 6 0 12 3 6"
-                fill={ hoveredItem != null || selectedItem != null ? "#505050" : "#D4D4D4"}
+                fill={
+                  hoveredItem != null || selectedItem != null
+                    ? "#505050"
+                    : "#D4D4D4"
+                }
               />
             </marker>
           </defs>
 
           <path
             className="arc"
-            d={`M ` + `${x0}` + ` ` + `${y0}` + ` Q ` + `${x1}` + ` ` + `${y1}` + ` ` + `${x2}` + ` ` + `${y2}`}
+            d={
+              `M ` +
+              `${x0}` +
+              ` ` +
+              `${y0}` +
+              ` Q ` +
+              `${x1}` +
+              ` ` +
+              `${y1}` +
+              ` ` +
+              `${x2}` +
+              ` ` +
+              `${y2}`
+            }
             fill="transparent"
-            stroke={ hoveredItem != null || selectedItem != null ? "#505050" : "#D4D4D4"}
-            strokeWidth="4"
+            stroke={
+              hoveredItem != null || selectedItem != null
+                ? "#505050"
+                : "#D4D4D4"
+            }
+            strokeWidth="2"
             markerEnd={`url(#triangle-${index})`}
           />
-
         </g>
       </svg>
-      {hoveredItem != null || selectedItem != null ?
+      {hoveredItem != null || selectedItem != null ? (
         <div className={"label"}>{label(selectedItem || hoveredItem)}</div>
-        : null
-      }
-    </div>)
-}
+      ) : null}
+    </div>
+  );
+};
 
-export default Arrow
-
+export default Arrow;
--- web/src/components/CommitteesChart/Committees.js
+++ web/src/components/CommitteesChart/Committees.js
@@ -1,28 +1,148 @@
-import React from 'react';
-import './Committees.css';
-import { useDispatch, useSelector } from "react-redux"
-import { ChangeHover, Select, Selected } from '../../state/state'
-import { isInCommittee } from "../../relation-helpers"
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
+import React from "react";
+import "./Committees.css";
+import { useDispatch, useSelector } from "react-redux";
+import { ChangeHover, Select, Selected } from "../../state/state";
+import { isInCommittee } from "../../relation-helpers";
+/*import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';*/
 
 const committeeData = [
-  { cx: 40, cy: 100, r: 25, color: "#999999", nameSv: "Arbetsmarknadsutskottet", shortName: "AU", description: "Labour Market Committee" },
-  { cx: 100, cy: 100, r: 25, color: "#999999", nameSv: "Civilutskottet", shortName: "CU", description: "Civil Affairs Committee" },
-  { cx: 160, cy: 100, r: 25, color: "#999999", nameSv: "Finansutskottet", shortName: "FiU", description: "Finance Committee" },
-  { cx: 220, cy: 100, r: 25, color: "#999999", nameSv: "Försvarsutskottet", shortName: "FöU", description: "Defence Committee" },
-  { cx: 280, cy: 100, r: 25, color: "#999999", nameSv: "Justitieutskottet", shortName: "JuU", description: "Justice Committee" },
-  { cx: 340, cy: 100, r: 25, color: "#999999", nameSv: "Konstitutionsutskottet", shortName: "KU", description: "Constitution Committee" },
-  { cx: 70, cy: 150, r: 25, color: "#999999", nameSv: "Kulturutskottet", shortName: "KrU", description: "Cutural Affairs Committee" },
-  { cx: 130, cy: 150, r: 25, color: "#999999", nameSv: "Miljö- och jordbruksutskottet", shortName: "MJU", description: "Environment and Agriculture Committee" },
-  { cx: 190, cy: 150, r: 25, color: "#999999", nameSv: "Näringsutskottet", shortName: "NU", description: "Industry and Trade Committee" },
-  { cx: 250, cy: 150, r: 25, color: "#999999", nameSv: "Skatteutskottet", shortName: "SkU", description: "Taxation Committee" },
-  { cx: 310, cy: 150, r: 25, color: "#999999", nameSv: "Socialförsäkringsutskottet", shortName: "SfU", description: "Social Insurance Committee" },
-  { cx: 100, cy: 200, r: 25, color: "#999999", nameSv: "Socialutskottet", shortName: "SoU", description: "Health and Welfare Committee" },
-  { cx: 160, cy: 200, r: 25, color: "#999999", nameSv: "Trafikutskottet", shortName: "TU", description: "Transport and Communication Committee" },
-  { cx: 220, cy: 200, r: 25, color: "#999999", nameSv: "Utbildningsutskottet", shortName: "UbU", description: "Education Committee" },
-  { cx: 280, cy: 200, r: 25, color: "#999999", nameSv: "Utrikesutskottet", shortName: "UU", description: "Foreign Affairs Committee" },
-]
+  {
+    cx: 40,
+    cy: 100,
+    r: 25,
+    color: "#999999",
+    nameSv: "Arbetsmarknadsutskottet",
+    shortName: "AU",
+    description: "Labour Market Committee"
+  },
+  {
+    cx: 100,
+    cy: 100,
+    r: 25,
+    color: "#999999",
+    nameSv: "Civilutskottet",
+    shortName: "CU",
+    description: "Civil Affairs Committee"
+  },
+  {
+    cx: 160,
+    cy: 100,
+    r: 25,
+    color: "#999999",
+    nameSv: "Finansutskottet",
+    shortName: "FiU",
+    description: "Finance Committee"
+  },
+  {
+    cx: 220,
+    cy: 100,
+    r: 25,
+    color: "#999999",
+    nameSv: "Försvarsutskottet",
+    shortName: "FöU",
+    description: "Defence Committee"
+  },
+  {
+    cx: 280,
+    cy: 100,
+    r: 25,
+    color: "#999999",
+    nameSv: "Justitieutskottet",
+    shortName: "JuU",
+    description: "Justice Committee"
+  },
+  {
+    cx: 340,
+    cy: 100,
+    r: 25,
+    color: "#999999",
+    nameSv: "Konstitutionsutskottet",
+    shortName: "KU",
+    description: "Constitution Committee"
+  },
+  {
+    cx: 70,
+    cy: 150,
+    r: 25,
+    color: "#999999",
+    nameSv: "Kulturutskottet",
+    shortName: "KrU",
+    description: "Cutural Affairs Committee"
+  },
+  {
+    cx: 130,
+    cy: 150,
+    r: 25,
+    color: "#999999",
+    nameSv: "Miljö- och jordbruksutskottet",
+    shortName: "MJU",
+    description: "Environment and Agriculture Committee"
+  },
+  {
+    cx: 190,
+    cy: 150,
+    r: 25,
+    color: "#999999",
+    nameSv: "Näringsutskottet",
+    shortName: "NU",
+    description: "Industry and Trade Committee"
+  },
+  {
+    cx: 250,
+    cy: 150,
+    r: 25,
+    color: "#999999",
+    nameSv: "Skatteutskottet",
+    shortName: "SkU",
+    description: "Taxation Committee"
+  },
+  {
+    cx: 310,
+    cy: 150,
+    r: 25,
+    color: "#999999",
+    nameSv: "Socialförsäkringsutskottet",
+    shortName: "SfU",
+    description: "Social Insurance Committee"
+  },
+  {
+    cx: 100,
+    cy: 200,
+    r: 25,
+    color: "#999999",
+    nameSv: "Socialutskottet",
+    shortName: "SoU",
+    description: "Health and Welfare Committee"
+  },
+  {
+    cx: 160,
+    cy: 200,
+    r: 25,
+    color: "#999999",
+    nameSv: "Trafikutskottet",
+    shortName: "TU",
+    description: "Transport and Communication Committee"
+  },
+  {
+    cx: 220,
+    cy: 200,
+    r: 25,
+    color: "#999999",
+    nameSv: "Utbildningsutskottet",
+    shortName: "UbU",
+    description: "Education Committee"
+  },
+  {
+    cx: 280,
+    cy: 200,
+    r: 25,
+    color: "#999999",
+    nameSv: "Utrikesutskottet",
+    shortName: "UU",
+    description: "Foreign Affairs Committee"
+  }
+];
 
 const committeeRadius = 25,
   committeeColors = {
@@ -42,29 +162,29 @@ const Committee = ({
   isHighlighted,
   onClick
 }) => (
-    <g
-      className={"committee"}
-      onMouseOver={onHover}
-      onMouseLeave={onLeaveHover}
-      onClick={onClick}
-    >
-      <circle
-        cx={cx}
-        cy={cy}
-        r={committeeRadius}
-        fill={
-          isHovered
-            ? committeeColors.hover
-            : isHighlighted
-              ? committeeColors.highlight
-              : committeeColors.background
-        }
-      />
-      <text x={cx} y={cy + 5} textAnchor={"middle"} fill={committeeColors.text}>
-        {shortName}
-      </text>
-    </g>
-  );
+  <g
+    className={"committee"}
+    onMouseOver={onHover}
+    onMouseLeave={onLeaveHover}
+    onClick={onClick}
+  >
+    <circle
+      cx={cx}
+      cy={cy}
+      r={committeeRadius}
+      fill={
+        isHovered
+          ? committeeColors.hover
+          : isHighlighted
+          ? committeeColors.highlight
+          : committeeColors.background
+      }
+    />
+    <text x={cx} y={cy + 5} textAnchor={"middle"} fill={committeeColors.text}>
+      {shortName}
+    </text>
+  </g>
+);
 
 const Committees = props => {
   const dispatch = useDispatch();
@@ -106,7 +226,9 @@ const Committees = props => {
           onClick={() => dispatch(Select(Selected.Committee(shortName)))}
         />
       ))}
-      <text x="190" y="260" fill="black" textAnchor={'middle'}>COMMITTEES</text>
+      <text x="190" y="260" fill="black" textAnchor={"middle"}>
+        COMMITTEES
+      </text>
     </svg>
   );
 };
